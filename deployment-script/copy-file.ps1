9b844ab8f40b76da81e80882dd12e0eab229a512
neilpeterson <neilpeterson@outlook.com>
2020-04-22 22:50:55 -0700

    demo ready

--- deployment-script/copy-file.ps1
+++ deployment-script/copy-file.ps1
@@ -1,3 +1,7 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/neilpeterson/2020-arm-talk/master/deployment-script/copy-file.sh -OutFile test.txt
 
+Invoke-WebRequest -Uri https://aka.ms/downloadazcopy-v10-windows -OutFile AzCopy.zip -UseBasicParsing
+
+Expand-Archive ./AzCopy.zip ./AzCopy -Force
+
 get-childitem
