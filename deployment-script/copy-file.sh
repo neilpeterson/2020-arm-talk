585561c4e03beb43ae2ef077f4c10b9790c3271b
neilpeterson <neilpeterson@outlook.com>
2020-04-22 22:11:50 -0700

    demo ready

--- deployment-script/copy-file.sh
+++ deployment-script/copy-file.sh
@@ -1,3 +1,3 @@
 curl https://raw.githubusercontent.com/neilpeterson/2020-arm-talk/master/deployment-script/copy-file.sh > test.txt
 
-echo "done"
+az group create --name from script --location eastus
